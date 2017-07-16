9a42a2f183a7fba94b0835a4f6c9f1902471158f
cho45 <cho45@lowreal.net>
2017-07-16 11:54:37 +0900

    ip

--- iotbotclient/plugins/core.rb
+++ iotbotclient/plugins/core.rb
@@ -1,8 +1,9 @@
 on_text("ステータス") do
 	uname  = `uname -a`.chomp
 	uptime = `uptime`.chomp
+	ifconfig = `ifconfig`[/inet addr:([^ ]+)/, 1]
 	cputemp = `vcgencmd measure_temp`.chomp rescue nil
-	reply_text("サービス稼働中\n uname: %s\n uptime: %s\n %s" % [uname, uptime, cputemp || "cpu: #{cputemp}"])
+	reply_text("サービス稼働中\n ip addr: %s\n uname: %s\n uptime: %s\n %s" % [ifconfig, uname, uptime, cputemp || "cpu: #{cputemp}"])
 end
 
 
